e2ecf778ecc09352b2d79a662dd1c9bbfcffdb0b
armiin98 <armin.geek.io@gmail.com>
2021-04-03 00:06:02 +0430

    installation file added

--- eye_care.sh
+++ eye_care.sh
@@ -7,10 +7,11 @@ fi
 export DISPLAY=127.0.0.1:0.0
 export XAUTHORITY=/home/$USER/.Xauthority
 
+
 while [ "i" == "i" ]
   do
     sleep 1m
-    /bin/play "/home/$USER/w/bash/eye_care_20_20_20/eye_care_song.mp3" &
+    /bin/play "/home/$USER/Downloads/eye_care_20_20_20/eye_care_song.mp3" &
     env DISPLAY=:0.0 /bin/notify-send -t 5000 "Look Atleast 20 Feet Away for 20 Seconds!"
   done
 
