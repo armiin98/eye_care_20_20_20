ba045a905dc00d6d34c51cd54e4e31782d89bef7
armiin98 <armin.geek.io@gmail.com>
2021-04-04 14:46:25 +0430

    bug fix

--- install.sh
+++ install.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# set cron to run app at startup
-crontab -l > .cron
-echo "@reboot /home/$USER/Downloads/eye_care_20_20_20/eye_care.sh" >> .cron
-crontab .cron
-rm .cron
-
 # set the DBUS_SESSION_BUS_ADDRESS variable. By default cron does not have access to the variable.
 mkdir $HOME/.dbus
 touch $HOME/.dbus/Xdbus
@@ -15,4 +9,10 @@ echo 'export DBUS_SESSION_BUS_ADDRESS' >> $HOME/.dbus/Xdbus
 
 # install sox (an app to play music from terminal)
 sudo pacman -S sox
+
+# set cron to run app at startup
+crontab -l > .cron
+echo "@reboot /home/$USER/Downloads/eye_care_20_20_20/eye_care.sh" >> .cron
+crontab .cron
+rm .cron
 
