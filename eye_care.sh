483bf29d362f58574a07369199ed7aa33992ea04
armiin98 <armin.geek.io@gmail.com>
2021-04-04 12:34:51 +0430

    cuple comments added

--- eye_care.sh
+++ eye_care.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+# chack DBUS_SESSION_BUS_ADDRESS variable
 if [ -r "$HOME/.dbus/Xdbus" ]; then
   . "$HOME/.dbus/Xdbus"
 fi
 
+# settings to display notification in screen
 export DISPLAY=127.0.0.1:0.0
 export XAUTHORITY=/home/$USER/.Xauthority
 
-
-while [ "i" == "i" ]
+while true
   do
-    sleep 4m
+    # run programme every 20 minutes
+    sleep 20m
+    #  play notification sound
     /bin/play "/home/$USER/Downloads/eye_care_20_20_20/eye_care_song.mp3" &
+    # show message
     env DISPLAY=:0.0 /bin/notify-send -t 5000 "Look Atleast 20 Feet Away for 20 Seconds!"
   done
 
